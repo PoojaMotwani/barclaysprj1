797c9606cc70c70527e363cc0449c127ead5ab0c
Your Name <you@example.com>
2019-08-23 14:07:56 +0530

    added calc program

---
+++ calc.py
@@ -0,0 +1,15 @@
+def add(x, y):
+	"""Add function"""
+	return x+y
+
+def substract(x, y):
+	"""Substract function"""
+	return x-y
+
+def multiply(x, y):
+	"""Multiply function"""
+	return x*y
+
+def division(x, y):
+	"""Division function"""
+	return x/y
